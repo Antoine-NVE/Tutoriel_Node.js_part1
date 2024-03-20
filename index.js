9d4623c99f3267e2faf73b5ea1deaac391b7f6dd
Antoine-NVE <antoinenavette@gmail.com>
2024-03-20 15:07:30 +0100

    Episode 10 : Changer la version de node et l'API Fetch

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const request = require('postman-request');
+// const request = require('postman-request');
 
-request('https://dog.ceo/api/breeds/image/random', (error, response, body) => {
-    const { message, status } = JSON.parse(body);
-    console.log(message);
-    console.log(status);
-});
+// request('https://dog.ceo/api/breeds/image/random', (error, response, body) => {
+//     const { message, status } = JSON.parse(body);
+//     console.log(message);
+//     console.log(status);
+// });
 
-request(
-    'https://api.thecatapi.com/v1/images/search',
-    (error, response, body) => {
-        const catPicture = JSON.parse(body);
-        console.log(catPicture[0].url);
-    }
-);
+// request(
+//     'https://api.thecatapi.com/v1/images/search',
+//     (error, response, body) => {
+//         const catPicture = JSON.parse(body);
+//         console.log(catPicture[0].url);
+//     }
+// );
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((response) => console.log(response.message));
+
+fetch('https://api.thecatapi.com/v1/images/search')
+    .then((response) => response.json())
+    .then((response) => console.log(response[0].url));
