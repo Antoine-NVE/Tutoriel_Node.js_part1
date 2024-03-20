f052931ef2eb44c4606415e0154c33595873d029
Antoine-NVE <antoinenavette@gmail.com>
2024-03-20 13:55:24 +0100

    Episode 7 : Comment utiliser le debugger de Node

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const { name, age, legal } = JSON.parse(readFileSync('data.json', 'utf-8'));
 // console.log(age);
 // console.log(legal);
 
-// const myData = {
-//     name: 'Justine',
-//     age: 27,
-//     legal: true,
-// };
-
-// const objectToJson = JSON.stringify(myData);
-
-// console.log(objectToJson);
-
-// writeFileSync('myData.json', objectToJson);
+const newData = {
+    name: 'Justine',
+    age: 27,
+    legal: true,
+};
+const objToJson = JSON.stringify(newData);
+writeFileSync('myData.json', objToJson);
 
 const myData = JSON.parse(readFileSync('myData.json', 'utf-8'));
 myData.name = 'Pierre';
 const objectToJson = JSON.stringify(myData);
 writeFileSync('myData.json', objectToJson);
+
+debugger;
+
+console.log(myData.name);
