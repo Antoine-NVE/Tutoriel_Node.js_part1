0d22a85c3293b269a011bef534031e229868f8e6
Antoine-NVE <antoinenavette@gmail.com>
2024-03-21 10:24:59 +0100

    Episode 16 : Découverte et utilisation des partials

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 3000; // 80
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,6 +20,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
+        name: 'Alex',
     });
 });
 
