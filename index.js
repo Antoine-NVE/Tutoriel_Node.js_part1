93ceb1cb4f7a6785b6892702a2cb6553fa37057b
Antoine-NVE <antoinenavette@gmail.com>
2024-03-21 08:50:30 +0100

    Episode 12 : Les données de plusieurs localisations

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const city = 'paris';
-
 // fetch(`https://www.prevision-meteo.ch/services/json/${city}`)
 //     .then((response) => response.json())
 //     .then((response) => {
@@ -8,18 +6,22 @@ const city = 'paris';
 //     });
 
 async function fetchWeather(city) {
-    response = await fetch(
-        `https://www.prevision-meteo.ch/services/json/${city}`
-    );
-    response = await response.json();
-    const { city_info, current_condition } = response;
-    // prettier-ignore
-    console.log(`
-        Le nom de la ville est ${city_info.name}.
-        Le pays est ${city_info.country}.
-        Il fait ${current_condition.tmp}° C.
-        Le temps est ${current_condition.condition.toLowerCase()}.
-    `);
-}
+    try {
+        response = await fetch(
+            `https://www.prevision-meteo.ch/services/json/${city}`
+        );
+        response = await response.json();
+        const { city_info, current_condition } = response;
 
-fetchWeather(city);
+        console.log(`
+            Le nom de la ville est ${city_info.name}.
+            Le pays est ${city_info.country}.
+            Il fait ${current_condition.tmp}° C.
+            Le temps est ${current_condition.condition}.
+        `); // prettier-ignore
+    } catch (error) {
+        console.log('Erreur lors de la récupération des données.', error);
+    }
+}
+fetchWeather('Paris');
+fetchWeather('Limoges');
