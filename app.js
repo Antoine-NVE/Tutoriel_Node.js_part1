8ddaab498e90f6d8eeed3dc8152de85cd308fc76
Antoine-NVE <antoinenavette@gmail.com>
2024-03-21 09:47:24 +0100

    Episode 15 : Découverte de notre template engine (hbars)

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const path = require('path');
+const { engine } = require('express-handlebars');
 const express = require('express');
+
 const app = express();
 const port = 3000; // 80
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home',
+        age: 30,
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+    });
+});
 
 app.listen(port, () => {
     console.log('App listening on port ' + port);
